f7ab8f048443b3b4d3b6e836e7a4fedd9b5a629f
Tiago Martins Araujo <tiago.martins.araujo@gmail.com>
2016-09-29 09:22:50 -0300

    delete action

--- app/routes/hotels.js
+++ app/routes/hotels.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.get('store').findAll('hotel');
+  },
+  actions: {
+    deleteHotel: function(hotel) {
+      hotel.destroyRecord().then(() => {
+        this.transitionTo('index');
+      });
+    }
   }
 });
